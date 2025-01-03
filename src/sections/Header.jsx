39c055baa9655d84201ba45d0f0d83055ab5decd
Keshav Aneja <119158790+Keshav-Aneja@users.noreply.github.com>
2025-01-03 21:40:37 +0530

    Update Header.jsx

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src="src/assets/logo.png" alt="Logo" className="logo" />
+        <img src="../assets/logo.png" alt="Logo" className="logo" />
       </div>
       <h1 className="header-title">Mozilla Firefox Club</h1>
       <button className="login-button">Login</button>
